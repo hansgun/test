283ba3e1ac37e393cf78ef12a4865860a4dd6596
hansgun <gmos@nate.com>
2018-10-18 17:27:54 +0900

    greeting

---
+++ greeting/src/main/java/com/example/demo/hello/controller/GreetingController.java
@@ -0,0 +1,26 @@
+package com.example.demo.hello.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.hello.vo.Greeting;
+
+@RestController
+public class GreetingController {
+	
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+	
+	@RequestMapping("/greeting")
+	public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+	
+	@RequestMapping("/")
+	public String test() {
+		return "1111!";
+	}
+}
